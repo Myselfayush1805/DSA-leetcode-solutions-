08413f28697c2515d1d80eabce879ee4e3b17e6d
Ayush Singh <ayushsingh0349@gmail.com>
2026-04-28 15:57:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.4 MB (85.85%)

--- my-folder/0289-game-of-life/solution.cpp
+++ my-folder/0289-game-of-life/solution.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
+        //METHOD 3(Ghost State Method)
         int n=board.size();
         int m=board[0].size();
-        vector<vector<int>>res=board;
+        int drow[]={-1,+1,0,0,-1,-1,+1,+1};
+        int dcol[]={0,0,-1,+1,-1,+1,-1,+1};
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 int count=0;
-                if(i-1>=0 && res[i-1][j]==1) count++;
-                if(i+1<n && res[i+1][j]==1) count++;
-                if(j-1>=0 && res[i][j-1]==1) count++;
-                if(j+1<m && res[i][j+1]==1) count++;
-                if(i-1>=0 && j-1>=0 && res[i-1][j-1]==1) count++;
-                if(i+1<n && j-1>=0 && res[i+1][j-1]==1) count++;
-                if(i-1>=0 && j+1<m && res[i-1][j+1]==1) count++;
-                if(i+1<n && j+1<m && res[i+1][j+1]==1) count++;
-                if(count<2 && board[i][j]==1) board[i][j]--;
-                // if(count>2 && board[i][j]==1) board[i][j];
-                if(count>3 && board[i][j]==1) board[i][j]--;
-                if(count==3 && board[i][j]==0) board[i][j]++; 
+                for(int k=0;k<8;k++){ 
+                    int nR=i+drow[k];
+                    int nC=j+dcol[k];                 
+                    if(nR>=0 && nR<n && nC>=0 && nC<m){
+                        if(board[nR][nC]==1 || board[nR][nC]==2) count++;
+                    }
+                }
+                if(board[i][j]==1){
+                    if(count<2 || count>3) board[i][j]=2;
+                }
+                else if(board[i][j]==0){
+                    if(count==3)  board[i][j]=3; 
+                }
             }
-        }        
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==2) board[i][j]=0;
+                else if(board[i][j]==3) board[i][j]=1;
+            }
+        }
     }
 };
