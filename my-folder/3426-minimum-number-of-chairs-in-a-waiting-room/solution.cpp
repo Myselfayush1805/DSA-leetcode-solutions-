59f32a4566c156f0353054c28e873354464b0961
Ayush Singh <ayushsingh0349@gmail.com>
2026-07-16 17:34:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.8 MB (58.50%)

---
+++ my-folder/3426-minimum-number-of-chairs-in-a-waiting-room/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumChairs(string s) {
+        int count=0;
+        int maxCount=0;
+        for(char c:s){
+            if(c=='E') count++;
+            else count--;
+            maxCount=max(maxCount,count);
+        }        
+        return maxCount;
+    }
+};
