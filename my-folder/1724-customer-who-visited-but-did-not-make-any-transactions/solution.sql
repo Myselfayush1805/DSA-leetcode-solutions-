e0c0566339aca5b99502e94c5081cd9ec63287b6
Ayush Singh <ayushsingh0349@gmail.com>
2026-08-04 12:26:41 +0000

    Sync LeetCode submission Runtime - 1668 ms (20.54%), Memory - 0.0B (100.00%)

---
+++ my-folder/1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT A.customer_id, COUNT(A.visit_id) AS count_no_trans
+FROM Visits A
+LEFT JOIN Transactions B ON A.visit_id=B.visit_id
+WHERE B.transaction_id IS NULL
+GROUP BY A.customer_id;
